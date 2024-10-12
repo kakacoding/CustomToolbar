319775e45979cfc659cf27ba125cccb7d25fc572
hubin <hubin@hypergryph.com>
2024-10-12 19:25:00 +0800

    update

---
+++ Editor/CustomToolbar/Common/ImageButtonCtrl.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace CustomToolbar.Editor
+{
+    public class ImageButtonToolbarCtrl : VisualElement
+    {
+	    internal delegate void Click();
+        internal static VisualElement Create(Click clickCallback)
+        {
+            var toolbarBtn = new ToolbarButton  
+            {
+            	tooltip = Tooltip,
+            };
+            toolbarBtn.AddToClassList(CustomToolbarUtility.TOOLBAR_BTN_MENU_INVOKE);
+
+            var bHasTex = false;
+            Texture2D tex = null;
+            if (!string.IsNullOrEmpty(TexturePath))
+            {
+            	tex = AssetDatabase.LoadAssetAtPath<Texture2D>(TexturePath);
+            	if (tex == null)
+            	{
+            		tex = AssetDatabase.LoadAssetAtPath<Texture2D>(CustomToolbarUtility.LOST_ICON);
+            	}
+            	bHasTex = tex != null;
+            }
+            var helpBox = new HelpBox(BtnText, HelpBoxMessageType.Info)
+            {
+            	style =
+            	{
+            		flexDirection = SettingDisplayType == CustomToolbarUtility.ETextTextureDisplay.TextTexture ? FlexDirection.Row : FlexDirection.RowReverse
+            	}
+            };
+            toolbarBtn.Add(helpBox);
+            var texFieldInHelpBox = helpBox.Children().FirstOrDefault(child => child is not Label);
+            if (texFieldInHelpBox != null)
+            {
+            	texFieldInHelpBox.style.backgroundImage = bHasTex ? tex : null;
+            	texFieldInHelpBox.style.minWidth = bHasTex ? texFieldInHelpBox.style.minHeight : 0;
+            	texFieldInHelpBox.style.maxWidth = bHasTex ? texFieldInHelpBox.style.maxHeight : 0;
+            }
+
+            toolbarBtn.style.width = string.IsNullOrEmpty(BtnText) ? 18 : BtnText.Length * 20 + (bHasTex ? 20 : 0);
+            toolbarBtn.clicked += () =>
+            {
+            	if (string.IsNullOrEmpty(MenuInvokePath))
+            	{
+            		CustomToolbarUtility.LogError(StrMenuNull);
+            	}
+            	else
+            	{
+            		EditorApplication.ExecuteMenuItem(MenuInvokePath);
+            		Counting();
+            	}
+            };
+        }
+    }
+}
---
+++ Editor/CustomToolbar/Common/TextureTextCtrl.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using PlasticGui.Configuration.CloudEdition.Welcome;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace CustomToolbar.Editor
+{
+    internal class TextureTextSettingCtrl : VisualElement
+    {
+        internal delegate CustomToolbarUtility.ETextTextureDisplay DisplayGetter();
+        internal delegate void DisplaySetter(CustomToolbarUtility.ETextTextureDisplay value);
+        
+        internal delegate string TextGetter();
+        internal delegate void TextSetter(string value);
+        internal delegate string TextureGetter();
+        internal delegate void TextureSetter(string value);
+        
+        internal static VisualElement Create(DisplayGetter displayGetter, DisplaySetter displaySetter, 
+            TextGetter textGetter, TextSetter textSetter, TextureGetter textureGetter, TextureSetter textureSetter)
+        {
+            var container = new VisualElement
+            {
+                style =
+                {
+                    flexDirection = FlexDirection.Row,
+                    alignContent = Align.FlexStart,
+                    alignItems = Align.Center,
+                    paddingBottom = 0,
+                    paddingTop =0,
+                    paddingLeft = 0,
+                    paddingRight = 0,
+                }
+            };
+            var enumField = new EnumField(CustomToolbarUtility.TTDisplayMap[CustomToolbarUtility.ETextTextureDisplay.TextureText], CustomToolbarUtility.ETextTextureDisplay.TextureText);
+            enumField.RegisterValueChangedCallback(evt =>
+            {
+                var newValue = (CustomToolbarUtility.ETextTextureDisplay)evt.newValue;
+                displaySetter(newValue);
+                enumField.label = CustomToolbarUtility.TTDisplayMap[newValue];
+                
+                var subContainer = container.Children().FirstOrDefault(child => child.name.Equals("subContainer"));
+                if (subContainer != null)
+                {
+                    subContainer.style.flexDirection = displayGetter() == CustomToolbarUtility.ETextTextureDisplay.TextureText ? FlexDirection.Row : FlexDirection.RowReverse;
+                }
+            });
+            enumField.AddToClassList(CustomToolbarUtility.SETTING_TEXT_TEXTURE_ENUM);
+            container.Add(enumField);
+            
+            var subContainer = new VisualElement
+            {
+                name = "subContainer",
+                style =
+                {
+                    flexDirection = FlexDirection.Row,
+                    alignContent = Align.Center,
+                    alignItems = Align.Center,
+                    width = StyleKeyword.Auto,
+                    paddingBottom = 0,
+                    paddingTop = 0,
+                    paddingLeft = 0,
+                    paddingRight = 0,
+                }
+            };
+            
+            var objField = new ObjectField
+            {
+                objectType = typeof(Texture2D),
+                value = AssetDatabase.LoadAssetAtPath<Texture2D>(textureGetter()),
+            };
+            objField.AddToClassList(CustomToolbarUtility.SETTING_TEXTURE_MIN);
+            objField.RegisterValueChangedCallback(evt =>
+            {
+                textureSetter(evt.newValue != null ? AssetDatabase.GetAssetPath(evt.newValue) : "");
+            });
+            subContainer.Add(objField);
+            
+            var txtBtnText = new TextField
+            {
+                value = textGetter(),
+            };
+            txtBtnText.AddToClassList(CustomToolbarUtility.SETTING_TEXT_MIN);
+            txtBtnText.RegisterCallback<ChangeEvent<string>>(evt =>
+            {
+                textSetter(evt.newValue);
+            });
+            subContainer.Add(txtBtnText);
+            
+            container.Add(subContainer);
+            return container;
+        }
+    }
+}
